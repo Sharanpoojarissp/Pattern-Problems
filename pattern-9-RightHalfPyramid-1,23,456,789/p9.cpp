c3da1afe11130977984a91e83bf9bd83f83d0624
Sharan S poojari <117771260+Sharanpoojarissp@users.noreply.github.com>
2023-04-27 16:56:51 +0530

    Add files via upload

---
+++ pattern-9-RightHalfPyramid-1,23,456,789/p9.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include <bits/stdc++.h>
+#include <cmath>
+using namespace std;
+
+int main(){
+
+     int n;
+    cout<<"Enter the number "<< endl;
+    cin >> n;
+    int count = 1;
+    int row = 1;
+    while(row <= n) {
+        int col = 1;
+        while(col <=row){
+            cout<<count << " ";
+            count ++ ;
+            col++ ;
+        }
+        cout<< endl;
+        row++ ;
+    }
+    return 0;
+}
